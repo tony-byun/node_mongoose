e57c9d066f4e9f2dbaf0c8ed336caa8fb2e17f2a
tony-byun <badblue37@gmail.com>
2018-08-06 16:12:47 +0900

    init router module

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+module.exports = function(app)  {
+    // get all books
+    app.get('/api/books', function(req, res) {
+        res.end();
+    });
+
+    // get single book
+    app.get('/api/books/:book_id', function(req, res) {
+        res.end();
+    });
+
+    // get book by author
+    app.get('/api/books/author/:author_id', function(req, res) {
+        res.end();
+    });
+
+    // create book
+    app.post('/api/books', function(req, res) {
+        res.end();
+    });
+
+    // update book
+    app.put('/api/books/:book_id', function(req, res) {
+        res.end();
+    });
+
+    // delete book
+    app.delete('/api/books/:book_id', function(req, res) {
+        res.end();
+    });
+}
